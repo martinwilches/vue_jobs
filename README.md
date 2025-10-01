79ab244ff6e345fef55cd284290f5194fd284329
Martin Wilches <martindotdev@gmail.com>
2025-09-30 21:55:02 -0500

    Inicializacion del proyecto

---
+++ README.md
@@ -0,0 +1,5 @@
+## Tools
+
+### tailwind
+
+[tailwind vite](https://tailwindcss.com/docs/installation/using-vite)
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Hero from './components/Hero.vue'
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+    <NavBar />
+    <Hero title="Test title" subtitle="Test subtitle" />
+    <HomeCards />
+    <JobListings />
+</template>
+
+<!-- scoped // los estilos solo se aplican en el componente actual -->
+<style scoped></style>
